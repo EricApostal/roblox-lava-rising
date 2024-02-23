67ee35b1d9d6b7deca5d0a9599e6526c8eafc3ac
EricApostal <ehapostal04@gmail.com>
2024-02-23 17:00:32 -0500

    made state work

---
+++ src/client/controllers/interface/ui.tsx
@@ -0,0 +1,24 @@
+import React, { StrictMode, useState } from "@rbxts/react";
+import { createPortal, createRoot } from "@rbxts/react-roblox";
+
+const playerGui = new Instance("ScreenGui", game.GetService("Players").LocalPlayer.WaitForChild("PlayerGui"));
+
+const root = createRoot(new Instance("Folder"));
+
+function Counter() {
+	const [count, setCount] = useState(0);
+
+	return (
+		<textbutton
+			Text={`Count: ${count}`}
+			AnchorPoint={new Vector2(0.5, 0.5)}
+			Size={new UDim2(0, 100, 0, 50)}
+			Position={new UDim2(0.5, 0, 0.5, 0)}
+			Event={{
+				Activated: () => setCount(count + 1),
+			}}
+		></textbutton>
+	);
+}
+
+root.render(<StrictMode>{createPortal(<Counter />, playerGui)}</StrictMode>);
--- src/client/controllers/ui.tsx
+++
@@ -1,6 +0,0 @@
-import React, { StrictMode } from "@rbxts/react";
-import { createPortal, createRoot } from "@rbxts/react-roblox";
-
-const root = createRoot(new Instance("Folder"));
-
-root.render(<textbutton>asd</textbutton>);
