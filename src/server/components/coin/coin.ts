0cca55aba2030415304aa6628e8f3c9288b85863
EricApostal <ehapostal04@gmail.com>
2024-02-24 16:43:08 -0500

    added local coin spin

---
+++ src/server/components/coin/coin.ts
@@ -0,0 +1,18 @@
+import { BaseComponent, Component } from "@flamework/components";
+import { OnStart } from "@flamework/core";
+
+@Component({tag: "coin"})
+export class CoinSpawn extends BaseComponent implements OnStart {
+    constructor() {
+        super();
+    }
+
+    onStart(): void {
+        assert(this.instance.IsA("Model"), "Coin component must be attached to a BasePart");
+
+        // while (true) {
+	    //     this.instance.PivotTo(this.instance.PrimaryPart!.CFrame.mul(CFrame.fromEulerAnglesXYZ(0, .1, 0)));
+	    //     wait()
+        // }
+    }
+}
--- src/server/components/coin/spawn.ts
+++ src/server/components/coin/spawn.ts
@@ -1,6 +1,7 @@
 import { BaseComponent, Component } from "@flamework/components";
 import { OnStart } from "@flamework/core";
 import { OnGameStarted } from "server/services/scheduler";
+import { ReplicatedStorage } from "@rbxts/services";
 
 @Component({tag: "coin-spawn"})
 export class CoinSpawn extends BaseComponent implements OnGameStarted {
@@ -9,6 +10,10 @@ export class CoinSpawn extends BaseComponent implements OnGameStarted {
     }
 
     onGameStarted(): void {
-        
+        if (!this.instance.IsA("BasePart")) return;
+
+        let coin: Model = ReplicatedStorage.FindFirstChild("assets")!.WaitForChild("coin").Clone() as Model;
+        coin.Parent = game.Workspace;
+        coin.MoveTo(this.instance.Position.add(new Vector3(0, 2, 0)));
     }
 }
