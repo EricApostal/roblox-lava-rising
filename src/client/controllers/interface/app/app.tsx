93933dffabcafb2e052741573a25e2ab45a44729
EricApostal <ehapostal04@gmail.com>
2024-02-23 19:04:43 -0500

    state works but it has weird incrementation

--- src/client/controllers/interface/app/app.tsx
+++ src/client/controllers/interface/app/app.tsx
@@ -1,24 +1,13 @@
 import React, { StrictMode, useState } from "@rbxts/react";
 import { createPortal, createRoot } from "@rbxts/react-roblox";
+import { Timer } from "./components/timer";
 
 const playerGui = new Instance("ScreenGui", game.GetService("Players").LocalPlayer.WaitForChild("PlayerGui"));
 
 const root = createRoot(new Instance("Folder"));
 
 function App() {
-	const [count, setCount] = useState(0);
-
-	return (
-		<textbutton
-			Text={`Count: ${count}`}
-			AnchorPoint={new Vector2(0.5, 0.5)}
-			Size={new UDim2(0, 100, 0, 50)}
-			Position={new UDim2(0.5, 0, 0.5, 0)}
-			Event={{
-				Activated: () => setCount(count + 1),
-			}}
-		></textbutton>
-	);
+	return <Timer />;
 }
 
 root.render(<StrictMode>{createPortal(<App />, playerGui)}</StrictMode>);
---
+++ src/client/controllers/interface/app/components/timer.tsx
@@ -0,0 +1,36 @@
+import React, { StrictMode, useState } from "@rbxts/react";
+import { createPortal, createRoot } from "@rbxts/react-roblox";
+
+const playerGui = new Instance("ScreenGui", game.GetService("Players").LocalPlayer.WaitForChild("PlayerGui"));
+
+const root = createRoot(new Instance("Folder"));
+
+export function Timer() {
+	const [time, setTime] = useState(0);
+
+	task.spawn(function () {
+		while (true) {
+			wait(1)
+			setTime(time + 1);
+		}
+	});
+
+	return (
+		<frame
+			Size={new UDim2(0, 200, 0, 75)}
+			Position={new UDim2(0.5, 0, 0, 0)}
+			AnchorPoint={new Vector2(0.5, 0)}
+			BorderSizePixel={0}
+			BackgroundColor3={new Color3(1, 1, 1)}
+			children={[
+				<uicorner CornerRadius={new UDim(0, 12)}></uicorner>,
+				<textlabel
+					Position={new UDim2(0.5, 0, 0.5, 0)}
+					AnchorPoint={new Vector2(0.5, 0)}
+					Text={`${time}`}
+					FontSize={Enum.FontSize.Size18}
+				/>,
+			]}
+		></frame>
+	);
+}
